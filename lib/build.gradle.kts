d10f7a1a4e9805f1ad7d3f9ad602c03443abaaf7
Edward Diehl <EdwDiehl@gmail.com>
2025-06-13 11:49:51 +0300

    Add ktlint plugin

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -12,6 +12,8 @@ plugins {
 
     // Apply the java-library plugin for API and implementation separation.
     `java-library`
+
+    id("org.jlleitschuh.gradle.ktlint") version "12.3.0"
 }
 
 repositories {
